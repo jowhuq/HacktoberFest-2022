0d7b7b4113feaacacda5dfcd9b40f93a7d9c5d88
jowhuq <116174880+jowhuq@users.noreply.github.com>
2022-10-20 19:34:39 +0530

    Add files via upload

---
+++ ALTERNATE METHOD FOR THE ABOVE PROGRAM.py
@@ -0,0 +1,2 @@
+x,y=(int(input("Enter Number1:")),int(input("Enter Number2:"))
+print("X=",x,"Y=",y)
---
+++ EXAMPLE FOR ANONYMOUS FUNCTIONS.py
@@ -0,0 +1,3 @@
+sum=lambda arg1,arg2:arg1+arg2
+print("The Sum Is:",sum(30,40))
+print("The Sum Is :",sum(-30,40))
